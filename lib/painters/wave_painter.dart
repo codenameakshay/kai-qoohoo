6724e059b0166381f4d606d5da888a60eea2c35d
codenameakshay <akshaymaurya3006@gmail.com>
2021-12-19 00:41:31 +0530

    add realistic data bool

--- lib/painters/wave_painter.dart
+++ lib/painters/wave_painter.dart
@@ -65,3 +65,60 @@ class WavePainter extends CustomPainter {
   @override
   bool shouldRebuildSemantics(WavePainter oldDelegate) => false;
 }
+
+class WavePainterOld extends CustomPainter {
+  final Color color;
+  final double heightPercentage;
+  final double amplitude;
+  final double phase;
+  final double waveFrequency;
+  WavePainterOld({
+    required this.color,
+    required this.heightPercentage,
+    required this.amplitude,
+    required this.phase,
+    required this.waveFrequency,
+  });
+  double viewWidth = 0.0;
+  double _tempA = 0.0;
+  double _tempB = 0.0;
+  final Paint _paint = Paint();
+  @override
+  void paint(Canvas canvas, Size size) {
+    double viewCenterY = size.height * (heightPercentage + 0.1);
+    viewWidth = size.width;
+    final path = Path();
+    path.reset();
+    path.moveTo(
+        0.0, viewCenterY + amplitude * _getSinY(phase, waveFrequency, -1));
+    for (int i = 1; i < size.width + 1; i++) {
+      path.lineTo(i.toDouble(),
+          viewCenterY + amplitude * _getSinY(phase, waveFrequency, i));
+    }
+    path.lineTo(size.width, size.height);
+    path.lineTo(0.0, size.height);
+    path.close();
+    _paint.color = color;
+    _paint.style = PaintingStyle.fill;
+    canvas.drawPath(path, _paint);
+  }
+
+  double _getSinY(
+      double startradius, double waveFrequency, int currentposition) {
+    if (_tempA == 0) {
+      _tempA = pi / viewWidth;
+    }
+    if (_tempB == 0) {
+      _tempB = 2 * pi / 360.0;
+    }
+
+    return (sin(
+        _tempA * waveFrequency * (currentposition + 1) + startradius * _tempB));
+  }
+
+  @override
+  bool shouldRepaint(WavePainterOld oldDelegate) => true;
+
+  @override
+  bool shouldRebuildSemantics(WavePainterOld oldDelegate) => false;
+}
--- lib/widgets/amplitude_widget.dart
+++ lib/widgets/amplitude_widget.dart
@@ -18,7 +18,7 @@ class _AmplitudeWidgetState extends State<AmplitudeWidget>
     with TickerProviderStateMixin {
   late AnimationController controller;
   late AnimationController amplitudeController;
-
+  bool realisticData = false;
   @override
   void initState() {
     super.initState();
@@ -57,22 +57,32 @@ class _AmplitudeWidgetState extends State<AmplitudeWidget>
     return Stack(
       children: [
         CustomPaint(
-          painter: WavePainter(
-            color: Theme.of(context).colorScheme.primary,
-            amplitudeHistory: _recordController.amplitude != null &&
-                    _recordController.amplitudeHistory.isNotEmpty
-                ? filter.smooth((_recordController.amplitudeHistory
-                    .map((e) => e * 80)).toList()) as List<double>
-                : List.filled(_recordController.lengthOfHistory, 20),
-            phase: _recordController.amplitude != null
-                ? 0
-                : 360 * controller.value,
-            heightPercentage: 0.53,
-            waveFrequency: (_recordController.amplitude != null &&
-                    _recordController.amplitudeHistory.isNotEmpty)
-                ? _recordController.amplitudeHistory.length.toDouble()
-                : 5,
-          ),
+          painter: realisticData
+              ? WavePainter(
+                  color: Theme.of(context).colorScheme.primary,
+                  amplitudeHistory: _recordController.amplitude != null &&
+                          _recordController.amplitudeHistory.isNotEmpty
+                      ? filter.smooth((_recordController.amplitudeHistory
+                          .map((e) => e * 80)).toList()) as List<double>
+                      : List.filled(_recordController.lengthOfHistory, 20),
+                  phase: _recordController.amplitude != null
+                      ? 0
+                      : 360 * controller.value,
+                  heightPercentage: 0.53,
+                  waveFrequency: (_recordController.amplitude != null &&
+                          _recordController.amplitudeHistory.isNotEmpty)
+                      ? _recordController.amplitudeHistory.length.toDouble()
+                      : 5,
+                )
+              : WavePainterOld(
+                  color: Theme.of(context).colorScheme.primary,
+                  amplitude: _recordController.amplitude != null
+                      ? (_recordController.amplitude ?? 0) * 80
+                      : (amplitudeController.value * 20),
+                  phase: 360 * controller.value,
+                  heightPercentage: 0.53,
+                  waveFrequency: _recordController.amplitude != null ? 10 : 5,
+                ),
           child: SizedBox(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height - kToolbarHeight,
