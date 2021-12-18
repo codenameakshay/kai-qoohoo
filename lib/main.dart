6346f10fe47a32ee2180275d5cf2972dc2ca8846
codenameakshay <akshaymaurya3006@gmail.com>
2021-12-18 18:19:12 +0530

    add flex_color_scheme, theme, and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:kai/constants/app_color.dart';
+import 'package:kai/constants/app_data.dart';
+import 'package:kai/controllers/theme_controller.dart';
 import 'package:kai/router/route_observer.dart';
 import 'package:kai/services/locator_service.dart';
 import 'package:kai/router/app_router.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupLocator();
+  final ThemeController themeController = ThemeController();
+  await themeController.init();
   runApp(
-    const MyApp(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => themeController,
+        ),
+      ],
+      child: const MyApp(),
+    ),
   );
 }
 
@@ -17,6 +31,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeController = context.watch<ThemeController>();
     final _appRouter = locator<AppRouter>();
     return MaterialApp.router(
       routerDelegate: AutoRouterDelegate(
@@ -25,9 +40,117 @@ class MyApp extends StatelessWidget {
       ),
       routeInformationParser: _appRouter.defaultRouteParser(),
       title: 'Kai',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: themeController.useFlexColorScheme
+          ? FlexThemeData.light(
+              colors: AppColor.scheme(themeController).light,
+              surfaceMode: themeController.surfaceMode,
+              blendLevel: themeController.blendLevel,
+              appBarStyle: themeController.lightAppBarStyle,
+              appBarOpacity: themeController.appBarOpacity,
+              appBarElevation: themeController.appBarElevation,
+              transparentStatusBar: themeController.transparentStatusBar,
+              tabBarStyle: themeController.tabBarStyle,
+              tooltipsMatchBackground: themeController.tooltipsMatchBackground,
+              swapColors: themeController.swapLightColors,
+              lightIsWhite: themeController.lightIsWhite,
+              useSubThemes: themeController.useSubThemes,
+              visualDensity: AppData.visualDensity,
+              fontFamily: AppData.font,
+              platform: themeController.platform,
+              subThemesData: FlexSubThemesData(
+                useTextTheme: themeController.useTextTheme,
+                defaultRadius: themeController.useDefaultRadius
+                    ? null
+                    : themeController.cornerRadius,
+                fabUseShape: themeController.fabUseShape,
+                interactionEffects: themeController.interactionEffects,
+                bottomNavigationBarOpacity:
+                    themeController.bottomNavigationBarOpacity,
+                bottomNavigationBarElevation:
+                    themeController.bottomNavigationBarElevation,
+                inputDecoratorIsFilled: themeController.inputDecoratorIsFilled,
+                inputDecoratorBorderType:
+                    themeController.inputDecoratorBorderType,
+                inputDecoratorUnfocusedHasBorder:
+                    themeController.inputDecoratorUnfocusedHasBorder,
+                blendOnColors: themeController.blendLightOnColors,
+                blendTextTheme: themeController.blendLightTextTheme,
+                popupMenuOpacity: 0.96,
+              ),
+            )
+          : ThemeData.from(
+              textTheme: ThemeData(
+                brightness: Brightness.light,
+              ).textTheme,
+              colorScheme: FlexColorScheme.light(
+                colors: AppColor.scheme(themeController).light,
+                surfaceMode: themeController.surfaceMode,
+                blendLevel: themeController.blendLevel,
+                swapColors: themeController.swapLightColors,
+                lightIsWhite: themeController.lightIsWhite,
+              ).toScheme,
+            ).copyWith(
+              visualDensity: AppData.visualDensity,
+              typography: Typography.material2018(
+                platform: themeController.platform,
+              ),
+            ),
+      darkTheme: themeController.useFlexColorScheme
+          ? FlexThemeData.dark(
+              colors: AppColor.scheme(themeController).dark,
+              surfaceMode: themeController.surfaceMode,
+              blendLevel: themeController.blendLevel,
+              appBarStyle: themeController.darkAppBarStyle,
+              appBarOpacity: themeController.appBarOpacity,
+              appBarElevation: themeController.appBarElevation,
+              transparentStatusBar: themeController.transparentStatusBar,
+              tabBarStyle: themeController.tabBarStyle,
+              tooltipsMatchBackground: themeController.tooltipsMatchBackground,
+              swapColors: themeController.swapDarkColors,
+              darkIsTrueBlack: themeController.darkIsTrueBlack,
+              useSubThemes: themeController.useSubThemes,
+              visualDensity: AppData.visualDensity,
+              fontFamily: AppData.font,
+              platform: themeController.platform,
+              subThemesData: FlexSubThemesData(
+                useTextTheme: themeController.useTextTheme,
+                defaultRadius: themeController.useDefaultRadius
+                    ? null
+                    : themeController.cornerRadius,
+                fabUseShape: themeController.fabUseShape,
+                interactionEffects: themeController.interactionEffects,
+                bottomNavigationBarOpacity:
+                    themeController.bottomNavigationBarOpacity,
+                bottomNavigationBarElevation:
+                    themeController.bottomNavigationBarElevation,
+                inputDecoratorIsFilled: themeController.inputDecoratorIsFilled,
+                inputDecoratorBorderType:
+                    themeController.inputDecoratorBorderType,
+                inputDecoratorUnfocusedHasBorder:
+                    themeController.inputDecoratorUnfocusedHasBorder,
+                blendOnColors: themeController.blendDarkOnColors,
+                blendTextTheme: themeController.blendDarkTextTheme,
+                popupMenuOpacity: 0.95,
+              ),
+            )
+          : ThemeData.from(
+              textTheme: ThemeData(
+                brightness: Brightness.dark,
+              ).textTheme,
+              colorScheme: FlexColorScheme.dark(
+                colors: AppColor.scheme(themeController).dark,
+                surfaceMode: themeController.surfaceMode,
+                blendLevel: themeController.blendLevel,
+                swapColors: themeController.swapDarkColors,
+                darkIsTrueBlack: themeController.darkIsTrueBlack,
+              ).toScheme,
+            ).copyWith(
+              visualDensity: AppData.visualDensity,
+              typography: Typography.material2018(
+                platform: themeController.platform,
+              ),
+            ),
+      themeMode: themeController.themeMode,
     );
   }
 }
--- lib/services/locator_service.dart
+++ lib/services/locator_service.dart
@@ -2,12 +2,15 @@ import 'package:get_it/get_it.dart';
 import 'package:kai/controllers/audio_player_controller.dart';
 import 'package:kai/controllers/path_controller.dart';
 import 'package:kai/controllers/record_controller.dart';
+import 'package:kai/controllers/theme_controller.dart';
 import 'package:kai/controllers/timer_controller.dart';
 import 'package:kai/router/app_router.dart';
 import 'package:kai/services/audio_player_service.dart';
 import 'package:kai/services/logger_service.dart';
 import 'package:kai/services/path_service.dart';
 import 'package:kai/services/record_service.dart';
+import 'package:kai/services/theme_pref_service.dart';
+import 'package:kai/services/theme_service.dart';
 import 'package:kai/services/timer_service.dart';
 
 GetIt locator = GetIt.instance;
@@ -18,7 +21,9 @@ void setupLocator() {
   locator.registerFactory<PathController>(() => PathController());
   locator.registerFactory<RecordController>(() => RecordController());
   locator.registerFactory<TimerController>(() => TimerController());
+  locator.registerFactory<ThemeController>(() => ThemeController());
   locator.registerSingleton<AppRouter>(AppRouter());
+  locator.registerLazySingleton<ThemeService>(() => ThemeServicePrefs());
   locator.registerLazySingleton<AudioPlayerService>(() => AudioPlayerService());
   locator.registerLazySingleton<PathService>(() => PathService());
   locator.registerLazySingleton<RecordService>(() => RecordService());
