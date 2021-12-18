f0c77e79691339e64313c65e68010d9c12e0799c
codenameakshay <akshaymaurya3006@gmail.com>
2021-12-18 17:03:45 +0530

    add audio_player_service

---
+++ lib/services/audio_player_service.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:just_audio/just_audio.dart';
+
+class AudioPlayerService {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  Stream<PlayerState> get playerStateStream => _audioPlayer.playerStateStream;
+  Stream<Duration> get playerPositionStream => _audioPlayer.positionStream;
+  Stream<Duration?> get playerDurationStream => _audioPlayer.durationStream;
+
+  // Set audio source
+  Future<void> setSource(String path) async {
+    final source = AudioSource.uri(Uri.parse(path));
+    await _audioPlayer.setAudioSource(source);
+  }
+
+  // Play audio
+  Future<void> play() async {
+    await _audioPlayer.play();
+  }
+
+  // Pause audio
+  Future<void> pause() async {
+    await _audioPlayer.pause();
+  }
+
+  // Stop audio
+  Future<void> stop() async {
+    await _audioPlayer.stop();
+  }
+
+  // Seek to a position
+  Future<void> seek(Duration position) async {
+    await _audioPlayer.seek(position);
+  }
+
+  // Get current position
+  Duration get position {
+    return _audioPlayer.position;
+  }
+
+  // Get duration
+  Duration? get duration {
+    return _audioPlayer.duration;
+  }
+
+  // Get audio player
+  AudioPlayer get audioPlayer {
+    return _audioPlayer;
+  }
+
+  // Dispose record in RecordService
+  void dispose() {
+    _audioPlayer.dispose();
+  }
+}
