273bd63d6b644b1dbd61f71f3cee4d65e2f5a967
codenameakshay <akshaymaurya3006@gmail.com>
2021-12-18 16:29:16 +0530

    Add timer_service

---
+++ lib/services/timer_service.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+class RecordService {
+  Timer? _timer;
+  Timer? _ampTimer;
+
+  // Start timer
+  void startTimer(Duration duration, Function() action) {
+    _timer?.cancel();
+    _timer = Timer.periodic(duration, (Timer t) {
+      action();
+    });
+  }
+
+  // Start amplitude timer
+  void startAmplitudeTimer(Duration duration, Function() action) {
+    _ampTimer?.cancel();
+    _ampTimer = Timer.periodic(duration, (Timer t) {
+      action();
+    });
+  }
+
+  // Cancel timer
+  void cancelTimer() {
+    _timer?.cancel();
+  }
+
+  // Cancel amplitude timer
+  void cancelAmplitudeTimer() {
+    _ampTimer?.cancel();
+  }
+
+  // Dispose timers
+  void dispose() {
+    _timer?.cancel();
+    _ampTimer?.cancel();
+  }
+}
