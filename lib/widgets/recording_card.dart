0ef68c541c0bbb2f473a9fa7f355812074fd2640
codenameakshay <akshaymaurya3006@gmail.com>
2021-12-19 18:48:06 +0530

    multiple audio streams playing

--- lib/widgets/recording_card.dart
+++ lib/widgets/recording_card.dart
@@ -2,7 +2,10 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:kai/controllers/audio_player_controller.dart';
 import 'package:path/path.dart' as p;
+import 'package:provider/provider.dart';
 
 class RecordingCard extends StatelessWidget {
   const RecordingCard({
@@ -29,24 +32,37 @@ class RecordingCard extends StatelessWidget {
     final int bitRate = int.parse(splitData[2].toString());
     final int samplingRate = int.parse(splitData[3].toString());
     final String fileFormat = splitData[4].split(".")[1];
-    return Card(
-      elevation: 0,
-      color: Theme.of(context).colorScheme.primaryVariant,
-      child: ListTile(
-        onTap: () {},
-        leading: const Icon(Icons.music_note),
-        title: Text(dateFormat.format(dateTime)),
-        subtitle: Text("${samplingRate / 1000}   ${fileFormat.toUpperCase()}"),
-        trailing: Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.secondary,
-              shape: BoxShape.circle,
-            ),
-            padding: const EdgeInsets.all(8),
-            child: Icon(
-              Icons.play_arrow,
-              color: Theme.of(context).colorScheme.onSecondary,
-            )),
+    return ChangeNotifierProvider<AudioPlayerController>(
+      create: (_) => AudioPlayerController(),
+      child: Card(
+        elevation: 0,
+        color: Theme.of(context).colorScheme.primaryVariant,
+        child: Consumer<AudioPlayerController>(builder: (context, apc, child) {
+          return ListTile(
+            onTap: () async {
+              await apc.setSource(file.path);
+              apc.play();
+            },
+            leading: const Icon(Icons.music_note),
+            title: Text(dateFormat.format(dateTime)),
+            subtitle:
+                Text("${samplingRate / 1000}   ${fileFormat.toUpperCase()}"),
+            trailing: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  shape: BoxShape.circle,
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Icon(
+                  apc.audioPlayer.playing &&
+                          apc.audioPlayer.processingState !=
+                              ProcessingState.completed
+                      ? Icons.pause
+                      : Icons.play_arrow,
+                  color: Theme.of(context).colorScheme.onSecondary,
+                )),
+          );
+        }),
       ),
     );
   }
